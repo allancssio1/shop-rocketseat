59e4699e8f5fc1b8ac90f0cb8ec511902436ee42
allancassio <allan.cassio1@gmail.com>
2023-11-11 05:38:12 -0300

    config stitches for to create css on server

---
+++ src/styles/index.ts
@@ -0,0 +1,10 @@
+import { createStitches } from '@stitches/react'
+
+export const { config, styled, css, globalCss, keyframes, getCssText, theme } =
+  createStitches({
+    theme: {
+      colors: {
+        primary: '#8257e6',
+      },
+    },
+  })
