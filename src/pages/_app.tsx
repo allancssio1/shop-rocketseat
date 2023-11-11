6baa31dcc381b3cb63611a7476b737a6f9841da9
allancassio <allan.cassio1@gmail.com>
2023-11-11 14:15:00 -0300

    styling header and container screen from pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import { AppProps } from 'next/app'
 import { globalStyles } from '../styles/globals'
+import logoImg from '../assets/logo.svg'
+import Image from 'next/image'
+import { Container, Header } from '../styles/pages/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        {/* <Image src={logoImg} alt="logo" /> */}
+        <img src={logoImg.src} alt="logo" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
