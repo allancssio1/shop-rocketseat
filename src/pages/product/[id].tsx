ed885c540185cc8d904036da347c7b3d60a7457c
allancassio <allan.cassio1@gmail.com>
2023-11-14 20:25:16 -0300

    implement css on product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,34 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContiner,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
+import Image from 'next/image'
+
+import camiseta from '../../assets/1.png'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>{JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContiner>
+        <img src={camiseta.src} alt="" />
+      </ImageContiner>
+      <ProductDetails>
+        <h1>Camiseta</h1>
+        <span>R$ 79,9</span>
+
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem iusto
+          nesciunt, reprehenderit laboriosam, officiis ab facere suscipit iste
+          hic magni asperiores alias. Ea, quibusdam. Aliquam similique aperiam
+          molestiae aut veniam.
+        </p>
+
+        <button>comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
